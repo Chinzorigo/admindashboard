59bbdbb9ca2e44930ae01bdb66473465a4799cf9
Chinzorigo <chinzorig.o@gmail.com>
2023-01-25 00:08:58 +0800

    login form

---
+++ register-form/src/login/login.js
@@ -0,0 +1,22 @@
+import { useFormik } from "formik";
+
+function Login() {
+
+    
+
+    return (
+        <div>
+            <h3>Hello</h3>
+        </div>
+    )
+}
+
+export default Login;
+
+
+
+
+
+
+
+
